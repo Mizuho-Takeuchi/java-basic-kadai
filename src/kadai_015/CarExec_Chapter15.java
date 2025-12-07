d95cae3f034ecf1299f60fa65db75269d7fc61d8
Mizuho-Takeuchi <76909761+Mizuho-Takeuchi@users.noreply.github.com>
2025-12-08 00:38:15 +0900

    Delete src/kadai_015 directory

--- src/kadai_015/CarExec_Chapter15.java
+++
@@ -1,9 +0,0 @@
-package kadai_015;
-
-public class CarExec_Chapter15 {
-	public static void main(String[]args) {
-		Car_Chapter15 carChapter = new Car_Chapter15();
-		carChapter.changeGear(3);
-		carChapter.run();
-	}
-}
